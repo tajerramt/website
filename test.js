7d25f9c6b2d4ee612e508c0043c4b1560ba90c9b
El <37004179+jerram@users.noreply.github.com>
2020-03-02 19:54:07 -0600

    tzs1

--- test.js
+++ test.js
@@ -4,63 +4,63 @@ var assert = require('assert');
 mongoose.Promise = global.Promise;
 
 describe('Database Tests', function () {
-//Before starting the test, create a sandboxed database connection
-//Once a connection is established invoke done()
-before(done => {
-    mongoose.connect('mongodb+srv://root:root@website-2j6up.mongodb.net/mocha?retryWrites=true&w=majority', {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
+    //Before starting the test, create a sandboxed database connection
+    //Once a connection is established invoke done()
+    before(done => {
+        mongoose.connect('mongodb+srv://root:root@website-2j6up.mongodb.net/mocha?retryWrites=true&w=majority', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
 
-    });
-    var db = mongoose.connection;
-    db.on('error', console.error.bind(console, 'connection error:'));
-    db.once('open', () => {
-
-        console.log("we're connected!");
-        // done();
-    });
-});
-describe('Test Database', function () {
-    //Save object with 'name' value of 'Mike"
-    it('New row saved to test database', function (done) {
-        var testName = property({
-            title: 'titlew',
-            unit: 'unitw[i]',
-            slug: 'slugw[i]',
-            address: 'addressw[i]',
-            description: 'descriptionw[i]',
-            numRoom: 1,
-            numBath: 1,
-            petFriendly: false,
-            ameneties: 'amenetiesw[i]',
-            media: 'mediaw[i]',
-            reviews: 'reviewsw[i]'
         });
+        var db = mongoose.connection;
+        db.on('error', console.error.bind(console, 'connection error:'));
+        db.once('open', () => {
 
-        testName.save(done);
-    });
-
-    it('Should retrieve data from test database', function (done) {
-        //Look up the 'Mike' object previously saved.
-        property.find({
-            title: 'titlew'
-        }, (err, name) => {
-            if (err) {
-                throw err;
-            }
-            if (name.length === 0) {
-                throw new Error('No data!');
-            }
-            // done();
+            console.log("we're connected!");
+            done();
         });
     });
-});
-// After all tests are finished drop database and close connection
-after(function (done) {
-    // mongoose.connection.db.dropDatabase(function () {
-    mongoose.connection.close(done);
-});
-});
+    describe('Test Database', function () {
+        //Save object with 'name' value of 'Mike"
+        it('New row saved to test database', function (done) {
+            var testName = property({
+                title: 'titlew',
+                unit: 'unitw[i]',
+                slug: 'slugw[i]',
+                address: 'addressw[i]',
+                description: 'descriptionw[i]',
+                numRoom: 1,
+                numBath: 1,
+                petFriendly: false,
+                ameneties: 'amenetiesw[i]',
+                media: 'mediaw[i]',
+                reviews: 'reviewsw[i]'
+            });
+
+            testName.save(done);
+        });
+
+        it('Should retrieve data from test database', function (done) {
+            //Look up the 'Mike' object previously saved.
+            property.find({
+                title: 'titlew'
+            }, (err, name) => {
+                if (err) {
+                    throw err;
+                }
+                if (name.length === 0) {
+                    throw new Error('No data!');
+                }
+                done();
+            });
+        });
+    });
+    // After all tests are finished drop database and close connection
+    after(function (done) {
+        mongoose.connection.db.dropDatabase(function () {
+            mongoose.connection.close(done);
+        });
+    });
 
 });
 
